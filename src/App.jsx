a7eb0f27800d675d7fc1b963fbbafb274be730c8
Amjad Hossain <amjad.bu.physics@gmail.com>
2024-12-19 12:07:58 +0600

    initialize the weather dashboard project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <p className="text-black font-medium text-xl underline-offset-4 underline">
+        Weather Dashboard Project
+      </p>
+    </>
+  )
+}
+
+export default App
